bf0266f004f9df63db03be59244f397da1bb96a3
andre <andre@FONMU-42UM2G1A5>
2023-12-24 16:28:38 +0100

    Add project files.

---
+++ QuickSwitchMod/Patches/InventoryControllerBPatches.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+
+
+namespace InvHotkeys.Patches
+{
+	[HarmonyPatch(typeof(PlayerControllerB))]
+	internal class InventoryControllerBPatch
+	{
+
+		[HarmonyPatch("Update")]
+		[HarmonyPostfix]
+		static void FastSwitchPatch(ref float ___timeSinceSwitchingSlots)
+		{
+			___timeSinceSwitchingSlots = 1f;
+		}
+
+
+	}
+}
---
+++ QuickSwitchMod/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using InvHotkeys.Patches;
+
+
+namespace InvHotkeys
+{
+	[BepInPlugin(modGUID, modName, modVersion)]
+	public class InvHotkeysBase : BaseUnityPlugin
+	{
+		private const string modGUID = "Asuran.InvHotkeys";
+		private const string modName = "Inventory Hotkeys";
+		private const string modVersion = "1.0.0";
+
+		private readonly Harmony harmony = new Harmony(modGUID);
+
+		private static InvHotkeysBase instance;
+
+		internal ManualLogSource mls;
+
+
+		void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this;
+			}
+
+			mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+			mls.LogInfo("InvHotkey mod has spawned.");
+
+			harmony.PatchAll(typeof(InvHotkeysBase));
+			harmony.PatchAll(typeof(InventoryControllerBPatch));
+		}
+
+	}
+}
